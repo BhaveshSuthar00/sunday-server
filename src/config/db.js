c7a29d9a15320ca635fc6682aad54ab746621a86
BhaveshSuthar00 <bhaveshs0770@gmail.com>
2022-05-06 20:48:46 +0530

    added the password

--- src/config/db.js
+++ src/config/db.js
@@ -2,9 +2,6 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const connect = async () => {
     return mongoose.connect(process.env.MONGODBURL)
-    .catch((e)=>{
-        console.log(e)
-    })
 }
 
 module.exports = connect
